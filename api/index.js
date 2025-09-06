3ba0d6e8e566ebf0539fd39525ec8b480b8c9211
JITENDRA KUMAR RANA <guddujitendrarana@gmail.com>
2025-09-06 10:05:10 +0530

    error resolved

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(
    cors({
       credentials: true,
       // origin: "http://localhost:5173",
-         origin: "http://jocular-gecko-e029e6.netlify.app",
+         origin: "https://jocular-gecko-e029e6.netlify.app",
    })
 );
 
